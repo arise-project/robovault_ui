8b85a0e86cd0131b81f5555920c8bb4a03c0f5b2
arise-project <pirogov.e@gmail.com>
2020-02-08 16:26:50 -0500

    create child router

--- src/js/viewModels/customers.js
+++ src/js/viewModels/customers.js
@@ -1,12 +1,63 @@
-define(['knockout', 'ojs/ojbootstrap', 'ojs/ojmodel', 'ojs/ojknockouttemplateutils', 'ojs/ojcollectiondatagriddatasource',
+define(['ojs/ojrouter','knockout', 'ojs/ojbootstrap', 'ojs/ojmodel', 'ojs/ojknockouttemplateutils', 'ojs/ojcollectiondatagriddatasource',
     'ojs/ojconverter-datetime', 'ojs/ojconverter-number', 'ojs/ojknockout', 'ojs/ojdatagrid'],
-  function (ko, Bootstrap, Model, KnockoutTemplateUtils,
+  function (Router, ko, Bootstrap, Model, KnockoutTemplateUtils,
     collectionModule, DateTimeConverter, NumberConverter) {
     var ViewModel = function () {
       this.KnockoutTemplateUtils = KnockoutTemplateUtils;
       var dateOptions = { formatType: 'date', dateFormat: 'medium' };
       this.dateConverter = new DateTimeConverter.IntlDateTimeConverter(dateOptions);
-  
+
+      var chapters = {
+        'preface': 'Darn beamed hurriedly because banal more \
+giraffe shuffled and well rid placidly where hence or and and hound lantern cutely \
+instead inaudibly but demonstrable imitatively one regarding a where much fruitlessly \
+more depending goodness less as dear shark directed this one.',
+        'chapter1': 'Affectingly and yikes one that along \
+versus growled unwitting vulnerably fish far pouting otter some as this hamster \
+hatchet where amicably far deftly curtsied.',
+        'chapter2': 'More up mistaken for a kissed therefore \
+ahead thus on dear wow undertook flabbily less much far sourly impala resolutely and \
+and as overheard dachshund this.',
+        'chapter3': 'Reindeer up while the far darn falcon \
+concurrent iguana this strived unicorn hedgehog depending more lemming was swam \
+unlike prosperously regarding shameful when and extravagant that then cat contagious.'
+     };
+
+      var parentRouter = Router.rootInstance;
+      this.router = parentRouter.getChildRouter('chapter') || parentRouter.createChildRouter('chapter')
+      .configure({
+         'preface': {
+            label: 'All',
+            value: chapters['preface'],
+            canExit: true
+         },
+         'chapter1': {
+            label: 'Chapter 1',
+            value: chapters['chapter1'],
+            canExit: true
+         },
+         'chapter2': {
+            label: 'Chapter 2',
+            value: chapters['chapter2'],
+            canExit: true
+         },
+         'chapter3': {
+            label: 'Chapter 3',
+            value: chapters['chapter3'],
+            canExit: true
+         }
+      });
+
+      var childRouter = this.router;
+      this.selectHandler = function(event) {
+        if ('menu' === event.target.id && event.detail.originalEvent) {
+           // router takes care of changing the selection
+           event.preventDefault();
+           // Invoke go() with the selected item.
+           childRouter.go(event.detail.key);
+        }
+     }
+
       var salaryOptions =
         {
           style: 'currency',
